79c4fe8fdef1eb34e75d791bffcaa702ccbc4aa1
akuma-dhruv <dhruvgarg79@gmail.com>
2021-06-27 22:27:03 +0530

    e 5

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var touchstartX = 0;
 var touchstartY = 0;
 var touchendX = 0;
 var touchendY = 0;
-
+var pos = 1;
 const obj = document.querySelector(".thing");
 if (screen.width < 1000) {
 
@@ -26,10 +26,43 @@ window.addEventListener('touchend', function (event) {
     handleGesture();
 }, false);
 
+function handleKeyResponse(e) {
+    moveObj(e.key);
+}
+
+function doSomething() {
+    var x, y;
+    setInterval(function d()  {
+        x = parseInt(obj.style.left);
+        y = parseInt(obj.style.top);
+        if (!x)
+            x = 0;
+        if (!y)
+            y = 0;
+    
+        switch (pos) {
+
+            case 0: obj.style.top = y - 50 + 'px';
+                break;
+            case 1: obj.style.left = x + 50 + 'px';
+                break;
+            case 2: obj.style.top = y + 50 + 'px';
+                break;
+            case 3: obj.style.left = x - 50 + 'px';
+                break;
+            default: console.log('default called');
+            }
+
+    }, 300);
+}
+function stopSomething(){
+    pos=4;
+}
+
 function handleGesture() {
     // console.log("handle gesture::i have been called check ");
 
-    if (touchendY === touchstartY && touchendX=== touchstartX) {
+    if (touchendY === touchstartY && touchendX === touchstartX) {
         //'Tap'
     }
     else if (Math.abs(touchendX - touchstartX) > Math.abs(touchendY - touchstartY)) {
@@ -37,15 +70,13 @@ function handleGesture() {
             //'Swiped left'
             moveObj('ArrowLeft');
         }
-        
+
         else if (touchendX > touchstartX) {
             //'Swiped right'
             moveObj('ArrowRight');
         }
     }
     else {
-
-
         if (touchendY < touchstartY) {
             //'Swiped up'
             moveObj('ArrowUp');
@@ -59,40 +90,51 @@ function handleGesture() {
         }
     }
 }
-function handleKeyResponse(e) {
-    moveObj(e.key);
-}
+// function moveObj(d) {
+//     var x = parseInt(obj.style.left);
+//     var y = parseInt(obj.style.top);
+//     var yh = screen.height - 315;
+//     var xw = screen.width;
 
+//     if (!x)
+//         x = 0;
+//     if (!y)
+//         y = 0;
+//     if (d) {
+//         switch (d) {
+//             case "ArrowUp":
+//                 if (y > -50)
+//                     obj.style.top = y - 50 + 'px';
+//                 else
+//                     obj.style.top = -85 + 'px';
+//                 break;
+//             case "ArrowDown":
+//                 if (y < yh) {
+//                     obj.style.top = y + 50 + 'px';
+//                 }
+//                 break;
+//             case "ArrowLeft": if (x - 50 >= 16) {
+//                 obj.style.left = x - 50 + 'px';
+//             } else {
+//                 obj.style.left = 16 + 'px';
+//             }
+//                 break;
+//             case "ArrowRight":
+//                 if (x < (xw - 100)) {
+//                     obj.style.left = x + 50 + 'px';
+//                 }
+//                 break;
+//         }
+//     }
+// }
 function moveObj(d) {
-    var x = parseInt(obj.style.left);
-    var y = parseInt(obj.style.top);
-    var yh = screen.height - 315;
-    var xw = screen.width;
 
-    if (!x)
-        x = 0;
-    if (!y)
-        y = 0;
     if (d) {
         switch (d) {
-            case "ArrowUp": if (y > -50) obj.style.top = y - 50 + 'px'; else obj.style.top = -85 + 'px'; break;
-            case "ArrowDown":
-                if (y < yh) {
-                    obj.style.top = y + 50 + 'px';
-                    console.log("if triggered");
-                }
-                break;
-            case "ArrowLeft": if (x - 50 >= 16) {
-                obj.style.left = x - 50 + 'px';
-            } else {
-                obj.style.left = 16 + 'px';
-            }
-                break;
-            case "ArrowRight":
-                if (x < (xw - 100)) {
-                    obj.style.left = x + 50 + 'px';
-                }
-                break;
+            case "ArrowUp": pos = 0; break;
+            case "ArrowRight": pos = 1; break;
+            case "ArrowDown": pos = 2; break;
+            case "ArrowLeft": pos = 3; break;
         }
     }
 }
